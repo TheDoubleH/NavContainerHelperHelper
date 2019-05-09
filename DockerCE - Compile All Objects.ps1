86e06655ecd5840e17f805679a08e5b26970befa
Henrik Helgesen <henrik@helgesen.us>
2019-05-09 13:02:19 -0700

    Added Compile All Objects

---
+++ DockerCE - Compile All Objects.ps1
@@ -0,0 +1,11 @@
+﻿<#This script will compile all objects in a Dockerized NAV Database, using the details specified in the _settings.ps1 file. 
+Version 19.13.1
+#>
+
+. (Join-Path $PSScriptRoot '.\_Settings.ps1')
+
+Compile-ObjectsInNavContainer `
+    -sqlCredential $Credential `
+    -containerName $ContainerName `
+    -filter '' `
+    -verbose
